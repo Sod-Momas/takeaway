f95904e07eff4981203346165b65de6b09f89be3
Sod-Momas <sothereer@gmail.com>
2023-06-27 21:33:11 +0800

    add: WebConfig

---
+++ src/main/java/io/github/sodmomas/takeaway/config/WebConfig.java
@@ -0,0 +1,17 @@
+package io.github.sodmomas.takeaway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Sod-Momas
+ * @since 2023/6/27
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/hello").setViewName("hello");
+    }
+}
