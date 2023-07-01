31615ab66ed865071e95a7dc672c25925a7fc0a6
Sod-Momas <sothereer@gmail.com>
2023-07-01 19:02:26 +0800

    add: 添加日志，添加默认用户

--- src/main/java/io/github/sodmomas/takeaway/TakeawayApplication.java
+++ src/main/java/io/github/sodmomas/takeaway/TakeawayApplication.java
@@ -1,8 +1,11 @@
 package io.github.sodmomas.takeaway;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan
@@ -10,8 +13,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 public class TakeawayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TakeawayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TakeawayApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner afterStart(Environment env) {
+        return arg -> {
+            final Integer port = env.getProperty("server.port", int.class, 8080);
+            System.out.printf("手动进页面: http://localhost:%d/%n", port);
+        };
+    }
 
 }
---
+++ src/main/resources/db/migration/V2023.07.01__init-data.sql
@@ -0,0 +1,5 @@
+-- 初始化用户 ;
+-- bCrypt('password') => {bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG
+INSERT INTO users(username,password,enabled) VALUES
+('user','{bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG',true),
+('admin','{bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG',true);
