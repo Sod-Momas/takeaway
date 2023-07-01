d62259e2ffd4793664d0ebdef6b3e8617bfe52d0
Sod-Momas <sothereer@gmail.com>
2023-07-01 18:31:59 +0800

    fix: 修复数据库无法连接的问题

--- src/test/java/io/github/sodmomas/takeaway/TakeawayApplicationTests.java
+++ src/test/java/io/github/sodmomas/takeaway/TakeawayApplicationTests.java
@@ -3,11 +3,25 @@ package io.github.sodmomas.takeaway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.*;
+
 @SpringBootTest
 class TakeawayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testSqlConnect() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        final String jdbc = "jdbc:postgresql://localhost:5432/takeaway";
+        final Connection connection = DriverManager.getConnection(jdbc, "takeaway", "takeaway");
+        final PreparedStatement ps = connection.prepareStatement("SELECT 1");
+        final ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+            final int result = rs.getInt(1);
+            System.out.println(result);
+        }
+        rs.close();
+        ps.close();
+        connection.close();
+    }
 
 }
